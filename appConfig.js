95bd6ba2b60b9a3fa77875601942a73ebb1bf934
rajeskabol <rajeskabol@gmail.com>
2024-12-15 19:17:20 +0700

    Update appConfig.js

--- appConfig.js
+++ appConfig.js
@@ -1,10 +1,10 @@
 module.exports = {
-  title: "JUDUL",
-  tagline: "Coloring Me Free Coloring Page",
-  description: "Coloring Me Free Coloring Pag.",
-  keywords: "coloring,coloringkids,agc,cdom blog",
-  author: "Coloringme.biz.id",
-  favicon: "https://pugjs.org/images/favicon.ico",
+  title: "Rezept Vegan Aprikosen",
+  tagline: "Der krümelige, aber dennoch saftige Rührkuchen passt perfekt zum Nachmittags-Kaffee im Sommer oder Herbst!",
+  description: "Dieser vegane Aprikosenkuchen mit knusprigem Kokos-Topping ist ein einfacher veganer Kuchen, der mit wenigen Zutaten schnell zubereitet ist und mit jedem Obst, wie Mandarinen oder Pfirsichen lecker schmeckt! ",
+  keywords: "Blechkuchen vegan aprikose,Rührteig vegan Blech,Zwetschgenkuchen vegan Blech,Versunkener Pflaumenkuchen vegan,Zwetschgenkuchen vegan Dinkelmehl,Kuchen mit gefrorenen Aprikosen,Pflaumentarte vegan,Aprikosen Kokoskuchen",
+  author: "Milyader Muda",
+  favicon: "",
   imageCount: 20,
   removeBadWords: "porn,nude,sex",
 };
